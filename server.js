d82399984b52e60f9d12f095356c7b0d227f1f28
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-11-02 14:41:01 +0100

    added health checker to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 
-const { roundRobinHandler }=require('./utils/handler');
+const config=require('./config/config');
+const { roundRobinHandler,performHealthCheck }=require('./utils/handler');
 
 const app=express();
 const PORT=3000;
@@ -10,6 +11,18 @@ const PORT=3000;
   Load Balancer implementation
 
 */
+
+global.servers=config.servers.map(server=>({url:server,isHealthy:true}))
+
+/*
+ Servers health check
+*/
+
+// Perform initial health check
+performHealthCheck();
+
+// Schedule periodic health checks (every 5 minutes in this example)
+const healthCheckInterval = setInterval(performHealthCheck, 10 * 1000);
 
 app.use("*",roundRobinHandler());
 
