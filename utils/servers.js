28d58b4a04e650e82205d72a933227a2c3c3f2de
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-11-02 14:31:09 +0100

    Added route for health check

--- utils/servers.js
+++ utils/servers.js
@@ -8,5 +8,18 @@ const server2=express();
 server1.use((req,res,next)=>res.send('Server 1'));
 server2.use((req,res,next)=>res.send('Server 2'));
 
+const healthCheck=(serverName)=>(req, res)=>{
+    try {
+      //checks database connectivity, server health, and other resource.
+      res.status(200).send(`${serverName} is health`);
+    } catch (err) {
+      console.error(`${serverName}check fialed : ${err}`);
+      res.status(500).send(`${serverName} is health`);
+    }
+  }
+  
+server1.get('/health', healthCheck('Server 1'));
+server2.get('/health',healthCheck('Server 2'));
+
 server1.listen(PORT1,()=>console.log('Server 1 is running'));
 server2.listen(PORT2,()=>console.log('Server 2 is running'));
