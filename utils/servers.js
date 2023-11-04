f7054a54732e6a385271c861201294059039c80e
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-11-04 21:46:45 +0100

    Fixed issue on the routes

--- utils/servers.js
+++ utils/servers.js
@@ -1,19 +1,18 @@
 const express=require('express');
-
 // Create servers
 const PORT1=3001, PORT2=3002;
 const server1=express();
 const server2=express();
 
-server1.use((req,res,next)=>res.send('Server 1'));
-server2.use((req,res,next)=>res.send('Server 2'));
-
-const healthCheck=(serverName)=>(req, res)=>{
+const healthCheck=(serverName)=>(req, res,next)=>{
+  console.log('health check')
     try {
       //checks database connectivity, server health, and other resource.
+      
       res.status(200).send(`${serverName} is healthy`);
     } catch (err) {
       console.error(`${serverName}check fialed : ${err}`);
+      next(err);
       res.status(500).send(`${serverName} is healthy`);
     }
   }
@@ -21,5 +20,8 @@ const healthCheck=(serverName)=>(req, res)=>{
 server1.get('/health', healthCheck('Server 1'));
 server2.get('/health',healthCheck('Server 2'));
 
-server1.listen(PORT1,()=>console.log('Server 1 is running'));
-server2.listen(PORT2,()=>console.log('Server 2 is running'));
+server1.get('/',(req,res,next)=>res.send('Server 1'));
+server2.get('/',(req,res,next)=>res.send('Server 2'));
+
+server1.listen(PORT1,()=>console.log(`Server 1 is running on port ${PORT1}`));
+// server2.listen(PORT2,()=>console.log(`Server 1 is running on port ${PORT2}`));
