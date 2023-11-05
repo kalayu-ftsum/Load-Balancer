83e96f19ef5002c90ea2d97df88b2a5cb20d6004
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-11-05 12:18:17 +0100

    Update servers to contain response time

--- config/config.js
+++ config/config.js
@@ -4,12 +4,14 @@ module.exports = {
             url: "http://localhost:3001",
             weight: 1,
             currentWeight:0,
+            responseTime:0,
             isHealthy: true
         },
         {
             url: "http://localhost:3002",
             weight: 2,
             currentWeight:0,
+            responseTime:0,
             isHealthy: true
         },
     ]
