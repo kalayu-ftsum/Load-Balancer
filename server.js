7e47145db068847fd9621d8a8697737a00d35696
Kalayu Ftsum <62060989+kalayu-ftsum@users.noreply.github.com>
2024-01-08 14:41:51 +0100

    Update server.js
    updated port to use env port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const config=require('./config/config');
 const { roundRobinHandler,performHealthCheck, weightedRoundRobinHandler, ipHash, weightedResponseTimeHandler, weightedLeastConnectionsHandler }=require('./utils/handler');
 
 const app=express();
-const PORT=3000;
+const PORT= process.env.PORT || 3000;
 
 /* 
 
